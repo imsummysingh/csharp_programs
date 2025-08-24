bbbbc53555791b37406a058573c3c8560fa3791d
Summy Singh <summysingh86@gmail.com>
2025-08-24 23:51:39 +0530

    progs added

---
+++ Program_1_StaticField/Program.cs
@@ -0,0 +1,22 @@
+﻿public class User
+{
+    public string name;
+    public static int userCount = 0; // Static field to keep track of the number of User instances
+
+    public User(string name)
+    {
+        this.name = name;
+        userCount++; // Increment the static field whenever a new User is created
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Initial User Count: " + User.userCount); // Output the initial user count
+        User u1 = new User("Alice");
+        User u2 = new User("Bob");
+        Console.WriteLine("User Count after creating two users object: " + User.userCount); // Output the user count after creating two users        
+    }
+}
---
+++ Program_2_Encapsulation/Program.cs
@@ -0,0 +1,64 @@
+﻿public class player
+{
+    //private fields for data hiding in encapsulation
+    private int _health = 100;
+    private int _heal = 100;
+
+    //public properties to access private fields and to manage the damage
+    public void TakeDamage(int damage)
+    {
+        if (damage < 0)
+        {
+            Console.WriteLine("Damage cannot be negative.");
+            return; //exit the method
+        }
+
+        _health -= damage;
+        Console.WriteLine($"player took {damage} damage");
+
+        if (_health < 0)
+        {
+            _health = 0; //prevent health from going below zero
+        }
+
+        Console.WriteLine($"player's health is now {_health}");
+    }
+
+    //public properties to manage healing
+    public void Heal(int healAmount)
+    {
+        if(healAmount < 0)
+        {
+            Console.WriteLine("Heal amount cannot be negative.");
+            return; //exit the method
+        }
+
+        _health += healAmount;
+
+        if(_health > _heal)
+        {
+            _health = _heal; //prevent health from exceeding maximum heal
+        }
+
+        Console.WriteLine($"player healed {healAmount} health");
+        Console.WriteLine($"player's health is now {_health}");
+    }
+}
+
+public class Program()
+{
+
+   public static void Main(string[] args)
+    {
+        player player1 = new player();
+
+        //Test the TakeDamage method
+        player1.TakeDamage(20);
+        player1.TakeDamage(50);
+        player1.TakeDamage(40); // This should not reduce health below 0
+
+        //Test the Heal method
+        player1.Heal(30);
+        player1.Heal(80); // This should not exceed maximum heal
+    }
+}   
